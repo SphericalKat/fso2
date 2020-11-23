95005aceb87e3f98a6f6396ab4baf55ad53f9394
SphericalKat <amolele@gmail.com>
2020-11-24 01:33:55 +0530

    feat(routing): set up routes and templates for viewing files
    Signed-off-by: SphericalKat <amolele@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
+extern crate askama;
 extern crate dotenv;
 extern crate pretty_env_logger;
 extern crate rbatis;
-#[macro_use] extern crate log;
+#[macro_use]
+extern crate log;
 
 use std::{env, io::Result};
 
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 use rbatis::rbatis::Rbatis;
 
-
-#[get("/")]
-async fn health() -> impl Responder {
-    HttpResponse::Ok().body("OK")
-}
+pub mod api;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
@@ -35,8 +33,12 @@ async fn main() -> Result<()> {
 
     // Start listening
     info!("Listening on port {}", port);
-    HttpServer::new(|| App::new().service(health))
-        .bind(format!("0.0.0.0:{}", port))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(api::render_files)
+            .service(api::render_files_root)
+    })
+    .bind(format!("0.0.0.0:{}", port))?
+    .run()
+    .await
 }
